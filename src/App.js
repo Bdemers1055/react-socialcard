081543e7ea80ba98e07b04dcf027ac2463cccb1c
Justin Dennison <justin1dennison@gmail.com>
2018-07-10 18:55:13 -0400

    stubbed out social card component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 /* eslint-disable */
 import React, { Component } from 'react';
-
+import SocialCard from './SocialCard';
 
 class App extends Component {
   render() {
     return (
       <div>
-        Yayyyyy it works
+        <SocialCard />
       </div>
     );
   }
