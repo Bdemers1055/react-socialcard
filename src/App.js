fea67e9cafe4c2d77d4c4b8d67558193b2d81c40
bdemers1055 <bdemers1055@gmail.com>
2018-07-11 19:27:06 -0400

    styled the grid of cards

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 import React, { Component } from 'react';
 import SocialCard from './SocialCard';
+import "./App.css";
 
 class App extends Component {
   constructor(props){
@@ -11,14 +12,15 @@ class App extends Component {
         "http://placekitten.com/251/250",
         "http://placekitten.com/250/251",
         "http://placekitten.com/252/250",
-        "http://placekitten.com/250/252"
+        "http://placekitten.com/250/252",
+        "http://placekitten.com/250/249"
       ]
     };
   }
   render() {
     const cards = this.state.imageUrls.map((url) => <SocialCard imageSrc={url} key={url} />)
     return (
-      <div>
+      <div className="appContainer">
         {cards}
       </div>
     );
