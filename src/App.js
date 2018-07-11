d05c71b50ccc0fc95648275eeaf066bb0024a882
bdemers1055 <bdemers1055@gmail.com>
2018-07-11 19:05:57 -0400

    manage imgs used array methods to construct comps from urls

--- src/App.js
+++ src/App.js
@@ -3,10 +3,23 @@ import React, { Component } from 'react';
 import SocialCard from './SocialCard';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      imageUrls: [  
+        "http://placekitten.com/250/250",
+        "http://placekitten.com/251/250",
+        "http://placekitten.com/250/251",
+        "http://placekitten.com/252/250",
+        "http://placekitten.com/250/252"
+      ]
+    };
+  }
   render() {
+    const cards = this.state.imageUrls.map((url) => <SocialCard imageSrc={url} key={url} />)
     return (
       <div>
-        <SocialCard imageSrc="https://via.placeholder.com/250x250" />
+        {cards}
       </div>
     );
   }
