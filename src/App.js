df63128326f7229cadd57a530eba10a1c50d56f2
Justin Dennison <justin1dennison@gmail.com>
2018-07-10 20:46:40 -0400

    modified the styles to a little more pleasing

--- src/App.js
+++ src/App.js
@@ -7,10 +7,6 @@ class App extends Component {
     return (
       <div>
         <SocialCard />
-        <SocialCard />
-        <SocialCard />
-        <SocialCard />
-        <SocialCard />
       </div>
     );
   }
